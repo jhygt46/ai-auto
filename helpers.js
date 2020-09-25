73747d583afeb8998d92e3367fbacbf70203dc5e
jhygt46 <infolifeoperacional@gmail.com>
2020-09-25 16:41:53 -0300

    aa

--- helpers.js
+++ helpers.js
@@ -1,15 +1,69 @@
 
 
 module.exports = {
-    getPort: function (){
-        return 80;
+    compare_objetos: function(objs, obj, base = ""){
+        var obj_key = Object.keys(obj);
+        for(var i=0, ilen=objs.length; i<ilen; i++){
+            var el_key = Object.keys(objs[i]);
+            if(el_key.length == obj_key.length){
+                var x = 0;
+                for(var k in obj){ if(objs[i][k] !== undefined){ if(k == 'n' && base+objs[i][k] == obj[k] || k != 'n' && objs[i][k] == obj[k]){ x++ }}}
+                if(x == el_key.length){ return true }
+            }
+        }
+        return false;
     },
-    combinaciones: function(a, b){
-        if (a.toString() < b.toString()) return -1;
-        if (a.toString() > b.toString()) return 1;
-        return 0;
+    objeto_palabra: function(obj, n){
+        obj.n = obj.n.substring(n);
+        return obj;
     },
-    compare: function(a, b){
-        return (a == b) ? 1 : 0 ;
+    nueva_lista: function(lista, obj, lvl){
+
+        var res = [];
+        for(var i=0, ilen=lista.length; i<ilen; i++){
+            if(obj.n.substring(0, lvl) + lista[i].n != obj.n || lista[i].i != obj.i){
+                res.push(lista[i]);
+            }
+        }
+        return res;
+    },
+    objeto_tipo: function(obj){
+        if(obj.tipo == 1){
+            return { t: 1, n: obj.nombre, ops: [], v: 0 }
+        }
+        if(obj.tipo == 2){
+            return { t: 2, n: obj.nombre, ops: [], v: 0 }
+        }
+    },
+    objeto_opcion_tipo: function(obj, campo){
+        if(campo.tipo == 1){
+            return { n: obj.val, v: 0 }
+        }
+        if(campo.tipo == 2){
+            return { n: obj.val, v: 0 }
+        }
+    },
+    ca_add_campo: function(campos, cambio){
+        if(campos == undefined){ campos = [] }
+        campos.push(this.objeto_tipo(cambio));
+        return campos;
+    },
+    ca_add_opcion_campo: function(campos, cambio){
+        var pos = (cambio.pos == -1) ? campos.length - 1 : cambio.pos ;
+        campos[pos].ops.push(this.objeto_opcion_tipo(cambio, campos[pos]));
+        return campos;
+    },
+    filtroCambios: function(json, cambios){
+        for(var i=0, ilen=cambios.length; i<ilen; i++){
+            switch(cambios[i].acc){
+                case "add_campo":
+                    json['campos'] = this.ca_add_campo(json['campos'], cambios[i]);
+                    break;
+                case "add_opcion_campo":
+                    json['campos'] = this.ca_add_opcion_campo(json['campos'], cambios[i]);
+                    break;
+              }
+        }
+        return json;
     }
 };
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 var fs = require('fs');
 var helpers = require('./helpers');
 var config = JSON.parse(fs.readFileSync('./config.json'));
@@ -13,8 +11,8 @@ socket.on('connect', function() {
 });
 socket.onevent = function(packet){
     switch (packet.data[0]) {
-        case 'cambiar_precio':
-            cambiar_precio(packet.data[1]);
+        case 'cambiar_filtro':
+            cambioFiltro(packet.data[1]);
         break;
     }
 }
@@ -22,6 +20,16 @@ socket.on('disconnect', function() {
     console.log('Disconnected!');
 });
 */
+
+function cambioFiltro(obj){
+
+    var path = obj.n;
+    fs.access('./filtros/'+path, fs.F_OK, (err) => { 
+        if(!err){ fs.readFile('./filtros/'+path, (err, data) => { 
+            if(!err){ fs.writeFile('./filtros/'+path, JSON.stringify(helpers.filtroCambios(JSON.parse(data), obj.c)), (err) => { if(err){ console.error(err); return }}); 
+    }else{ console.error(err); return }}); }else{ console.error(err); return }})
+    
+}
 
 const express = require("express");
 const app = express();
@@ -34,61 +42,237 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var data = {};
+var limit = 4;
+
+//ADD PALABRA
+function add_palabra_force(obj, lvl, num = 0){
+
+    var l0 = obj.n[0];
+    var l1 = obj.n[1];
+    var rest = obj.n.substring(2);
+    
+    if(!data.hasOwnProperty(l0)){
+        data[l0] = {};
+    }
+    if(!data[l0].hasOwnProperty(l1)){
+        data[l0][l1] = {};
+    }
+    
+    switch(lvl){
+        case 0:
+            if(!data[l0][l1].hasOwnProperty('&')){
+                data[l0][l1]['&'] = [helpers.objeto_palabra(obj, 2)];
+            }else{
+                if(data[l0][l1]['&'].length < limit){
+                    data[l0][l1]['&'].unshift(helpers.objeto_palabra(obj, 2));
+                }else{
+                    var last = data[l0][l1]['&'].pop();
+                    data[l0][l1]['&'].unshift(helpers.objeto_palabra(obj, 2));
+                    add_palabra_force(last, lvl+1);
+                }
+            }
+        break;
+        case 1:
+            if(!data[l0][l1].hasOwnProperty(rest[0])){
+                data[l0][l1][rest[0]] = { '&': [helpers.objeto_palabra(obj, 3)] };
+            }else{
+                if(data[l0][l1][rest[0]]['&'].length < limit){
+                    data[l0][l1][rest[0]]['&'].unshift(helpers.objeto_palabra(obj, 3));
+                }else{
+                    var last = data[l0][l1][rest[0]]['&'].pop();
+                    data[l0][l1][rest[0]]['&'].unshift(helpers.objeto_palabra(obj, 3));
+                    add_palabra_force(last, lvl+1);
+                }
+            }
+        break;
+        case 2:
+            if(!data[l0][l1][rest[0]].hasOwnProperty(rest[0])){
+                data[l0][l1][rest[0]][rest[1]] = { '&': [helpers.objeto_palabra(obj, 4)] };
+            }else{
+                if(data[l0][l1][rest[0]][rest[1]]['&'].length < limit){
+                    data[l0][l1][rest[0]][rest[1]]['&'].unshift(helpers.objeto_palabra(obj, 4));
+                }else{
+                    var last = data[l0][l1][rest[0]][rest[1]]['&'].pop();
+                    data[l0][l1][rest[0]][rest[1]]['&'].unshift(helpers.objeto_palabra(obj, 4));
+                    add_palabra_force(last, lvl+1);
+                }
+            }
+        break;
+    }
+
+}
+function add_palabra(obj, lvl = 0){
+
+    var l0 = obj.n[0];
+    var l1 = obj.n[1];
+    var rest = obj.n.substring(2);
+    if(!data.hasOwnProperty(l0)){ data[l0] = {} }
+    if(!data[l0].hasOwnProperty(l1)){ data[l0][l1] = {} }
+
+    switch(lvl){
+        case 0:
+            if(!data[l0][l1].hasOwnProperty('&')){
+                data[l0][l1]['&'] = [helpers.objeto_palabra(obj, 2)];
+            }else{
+                if(data[l0][l1]['&'].length < limit){
+                    data[l0][l1]['&'].push(helpers.objeto_palabra(obj, 2));                        
+                }else{
+                    var last = data[l0][l1]['&'].pop();
+                    last.n = l0+l1+last.n;
+                    data[l0][l1]['&'].unshift(helpers.objeto_palabra(obj, 2));
+                    add_palabra_force(last, lvl+1);
+                }
+            }
+        break;
+        case 1:
+            if(!data[l0][l1].hasOwnProperty(rest[0])){
+                data[l0][l1][rest[0]] = { '&': [helpers.objeto_palabra(obj, 3)] };
+            }else{
+                if(data[l0][l1][rest[0]]['&'].length < limit){
+                    data[l0][l1][rest[0]]['&'].push(helpers.objeto_palabra(obj, 3));
+                }else{
+                    var last = data[l0][l1][rest[0]]['&'].pop();
+                    last.n = l0+l1+[rest[0]]+last.n;
+                    data[l0][l1][rest[0]]['&'].unshift(helpers.objeto_palabra(obj, 3));
+                    add_palabra_force(last, lvl+1);
+                }
+            }
+        break;
+        case 2:
+            if(!data[l0][l1][rest[0]].hasOwnProperty(rest[1])){
+                data[l0][l1][rest[0]][rest[1]] = { '&': [helpers.objeto_palabra(obj, 4)] };
+            }else{
+                if(data[l0][l1][rest[0]][rest[1]]['&'].length < limit){
+                    data[l0][l1][rest[0]][rest[1]]['&'].push(helpers.objeto_palabra(obj, 4));
+                }else{
+                    var last = data[l0][l1][rest[0]][rest[1]]['&'].pop();
+                    //data[l0][l1][rest[0]][rest[1]]['&'].unshift(helpers.objeto_palabra(obj, 2));
+                    //add_palabra_force(last, lvl+1);
+                }
+            }
+        break;
+    }
+    
+}
+function lista_palabras(base, i){
+    var palabra = base.substring(0, i);
+    switch (palabra.length) {
+        case 2:
+            try{ return data[palabra[0]][palabra[1]]['&'] }catch(e){ return [] }
+        break;
+        case 3:
+            try{ return data[palabra[0]][palabra[1]][palabra[2]]['&'] }catch(e){ return [] }
+        break;
+        case 4:
+            try{ return data[palabra[0]][palabra[1]][palabra[2]][palabra[3]]['&'] }catch(e){ return [] }
+        break;
+    }
+}
+function listas_palabras(palabra, palabra_ant){
+
+    var resp = [];
+    if(palabra_ant == undefined){ palabra_ant = palabra.substring(0, 1) }
+    for(var i=palabra_ant.length+1, ilen=palabra.length; i<=ilen; i++){
+        resp = resp.concat(lista_palabras(palabra, i));
+    }
+    return resp;
+
+}
+function get_palabra(obj){
+    for(var i=2, ilen=obj.n.length; i<=ilen; i++){
+        if(helpers.compare_objetos(lista_palabras(obj.n, i), obj, obj.n.substring(0, i))){
+            return { op: true, lvl: i };
+        }
+    }
+    return { op: false };
+}
+function delete_obj(obj){
+
+    var aux = get_palabra(obj);
+    var l0 = obj.n[0];
+    var l1 = obj.n[1];
+    var rest = obj.n.substring(2);
+
+    console.log(aux);
+
+    if(aux.op){
+        switch (aux.lvl) {
+            case 2:
+                data[l0][l1]['&'] = helpers.nueva_lista(lista_palabras(obj.n, aux.lvl), obj, aux.lvl);
+            break;
+            case 3:
+                data[l0][l1][rest[0]]['&'] = helpers.nueva_lista(lista_palabras(obj.n, aux.lvl), obj, aux.lvl);
+            break;
+            case 4:
+                data[l0][l1][rest[0]][rest[1]]['&'] = helpers.nueva_lista(lista_palabras(obj.n, aux.lvl), obj, aux.lvl);
+            break;
+            case 5:
+                data[l0][l1][rest[0]][rest[1]][rest[2]]['&'] = helpers.nueva_lista(lista_palabras(obj.n, aux.lvl), obj, aux.lvl);
+            break;
+        }
+    }
+
+}
+
+
+
 
 app.get('/', function(req, res){
 
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(helpers.compare("Rest", "Rest")));
+    var obj = { n: 'rest', p: 0 };
+    console.log(get_palabra(obj));
+    cambioFiltro({ n: "a", c: [{ acc: 'add_campo', tipo: 1, nombre: 'opciones' }, { acc: 'add_opcion_campo', pos: -1, val: 'BuEnA Enestor' }, { acc: 'add_opcion_campo', pos: -1, val: 'BuEnA BUENA' }] });
+    res.end("OK");
 
 });
+app.post('/get_filtro', function(req, res){
 
-var data = { "r": { "e": { "&": ["Buena", "Nelson"] } } };
+    res.setHeader('Content-Type', 'application/json');
+    var path = req.body.num;
+    fs.access('./filtros/'+path, fs.F_OK, (err) => { 
+    if(!err){ fs.readFile('./filtros/'+path, (err, data) => { 
+        if(!err){ res.end(data) }else{ console.error(err); return }
+    }); }else{ console.error(err); return }})
 
+});
+app.get('/filtro', function(req, res){
+
+    res.setHeader('Content-Type', 'application/json');
+    fs.access('./filtros/'+req.query.f, fs.F_OK, (err) => { 
+        if(!err){ 
+            fs.readFile('./filtros/'+req.query.f, (err, data) => { 
+                if(!err){ 
+                    res.end(data);
+                }else{ 
+                    res.end("Error");
+                }
+            })
+        }else{ 
+            res.end("Error");
+        }
+    });
+
+});
+app.get('/del', function(req, res){
+
+    res.setHeader('Content-Type', 'application/json');
+    var obj = { n: 'rest', i: 3 };
+    delete_obj(obj);
+    res.end(JSON.stringify(data, null, 4));
+
+});
 app.post('/ac', urlencodedParser, function(req, res){
 
     res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(listas_palabras(req.body.palabra, req.body.palabra_ant)));
     
-    var palabra = "re";
-    var palabra_ant = "re";
-    var peticiones = helpers.combinaciones(palabra, palabra_ant);
-    var nlen = palabra.length;
-
-    
-    switch (nlen) {
-        case 1:
-            var resp = data[palabra[0]]['&'];
-        break;
-        case 2:
-            var resp = data[palabra[0]][palabra[1]]['&'];
-        break;
-        case 2:
-            var resp = data[palabra[0]][palabra[1]][palabra[2]]['&'];
-        break;
-    }
-    
-
-    res.end(JSON.stringify(resp));
-
 });
-
 app.listen(config.port, () => {
-    fs.appendFile('init.log', 'Servidor iniciado a las ' + new Date().toLocaleString() + ' en puerto ' + config.port + '\n', function(err){ if(err) return console.log(err); console.log("SERVER START") });
+    fs.appendFile('init.log', 'Servidor iniciado a las ' + new Date().toLocaleString() + ' en puerto ' + config.port + '\n', (err) => { if(err) return console.log(err); console.log("SERVER START") });
+    var objs = [{ n: 're', i: 1 }, { n: 'res', i: 2 }, { n: 'rest', i: 3 }, { n: 'resta', i: 4 }, { n: 'restau', i: 5 }, { n: 'restaur', i: 6 }];
+    for(var i=0, ilen=objs.length; i<ilen; i++){
+        add_palabra(objs[i], 0);
+    }
 });
-/* 
-if(!data.hasOwnProperty(palabra[0])){ 
-    data[palabra[0]] = {}; 
-    if(nlen == 1){ 
-        data[palabra[0]]['&'] = id; 
-        return 0; 
-    }
-}else{ 
-    if(nlen == 1){ 
-        if(mails[domain][name[0]]['&'] == undefined){ 
-            mails[domain][name[0]]['&'] = id; 
-            return 0; 
-        }else{ 
-            return 1; 
-        }
-    }
-}
-*/
